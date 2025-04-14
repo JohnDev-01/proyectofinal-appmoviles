4a94bb15a65d29838adb5a9d6a82f8f4de1ba500
john silvestre <johnkerlin52@gmail.com>
2025-04-14 12:30:26 -0400

    feat: change start page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,17 @@
-﻿namespace proyectofinal_appmoviles;
+﻿using proyectofinal_appmoviles.Views;
+
+namespace proyectofinal_appmoviles;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+		MainPage = new LoginChoicePage();
 	}
 
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
-	}
+	//protected override Window CreateWindow(IActivationState? activationState)
+	//{
+	//	return new Window(new AppShell());
+	//}
 }
